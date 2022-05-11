6a072f8d5c1616eeac81ca1eb5de8e465a92fa64
agreer31 <agreer31@gatech.edu>
2022-05-10 22:11:11 -0400

    typo

--- movie_bot.py
+++ movie_bot.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-MONGO_PW = os.geteng("MONGO_PW")
+MONGO_PW = os.getenv("MONGO_PW")
 
 client = MongoClient(f"mongodb+srv://dipchest:{MONGO_PW}@greg.hrim0.mongodb.net/Greg?retryWrites=true&w=majority")
 db = client.movies
