001a988fe746cf6e834d9682a8579814919b3be6
agreer31 <agreer31@gatech.edu>
2022-05-11 12:10:07 -0400

    Fixes mongo client string bug

--- movie_bot.py
+++ movie_bot.py
@@ -13,7 +13,7 @@ if MONGO_PW == 'testing':
     mcl = 'localhost'
 else:
     mcl = f"mongodb+srv://dipchest:{MONGO_PW}@greg.hrim0.mongodb.net/Greg?retryWrites=true&w=majority"
-client = MongoClient("localhost")
+client = MongoClient(mcl)
 db = client.movies
 
 print(client.list_database_names())
